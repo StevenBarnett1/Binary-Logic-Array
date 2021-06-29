9f86a851184dd5e5bd006aac97de81e305850cca
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-06-29 14:17:35 -0700

    Finished some problems

--- dynamic-array/dynamic-array.js
+++ dynamic-array/dynamic-array.js
@@ -1,50 +1,54 @@
 class DynamicArray {
-
-  constructor(defaultSize=4) {
-
-    // Your code here
-
-    // Read specs for details
-
+  constructor(defaultSize = 4) {
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
+    if (index > this.length) return false;
 
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here
+    this.data[this.length] = val;
+    this.length++;
   }
 
-
   pop() {
+    let ele = this.data[this.length - 1];
 
-    // Your code here
+    this.data[this.length - 1] = null;
+    this.length--;
+
+    return ele;
   }
 
   shift() {
-
-    // Your code here
+    let ele = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.length--
+    return ele;
   }
 
   unshift(val) {
+    for (let i = this.data.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
 
+  indexOf(val) {
     // Your code here
   }
 
-  indexOf (val) {
-
+  resize() {
     // Your code here
   }
-
-  resize () {
-
-    // Your code here
-  }
-
 }
-
 
 module.exports = DynamicArray;
